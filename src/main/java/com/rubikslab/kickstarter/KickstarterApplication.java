21259fb062b1e213ba60254de2e3896de0110733
Lufun Nahar <rubycse@gmail.com>
2018-10-27 19:28:53 +0600

    Initial Commit: Created By Spring Initializer

---
+++ src/main/java/com/rubikslab/kickstarter/KickstarterApplication.java
@@ -0,0 +1,12 @@
+package com.rubikslab.kickstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickstarterApplication.class, args);
+	}
+}
