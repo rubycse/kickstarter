a7671933abb3e15469ecd869901e327b34ba5bdd
Lufun Nahar <rubycse@gmail.com>
2018-10-28 16:09:33 +0600

    Added database config from property file

--- src/main/java/com/rubikslab/kickstarter/KickstarterApplication.java
+++ src/main/java/com/rubikslab/kickstarter/KickstarterApplication.java
@@ -2,8 +2,12 @@ package com.rubikslab.kickstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@PropertySource(ignoreResourceNotFound = true, value = "file:${KICKSTARTER_PROP_LOC}")
 public class KickstarterApplication {
 
 	public static void main(String[] args) {
