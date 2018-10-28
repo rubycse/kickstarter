71fb3bc681687b3b57ff915ca9ad4f1f05aa12af
Lufun Nahar <rubycse@gmail.com>
2018-10-28 16:10:10 +0600

    Added home page

---
+++ src/main/java/com/rubikslab/kickstarter/WebConfig.java
@@ -0,0 +1,17 @@
+package com.rubikslab.kickstarter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author lutfun
+ * @since 10/28/18
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
